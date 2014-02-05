d0620018c1cb7fae63acb0a2c43cd16091e3f7ec
MrMcKizzle <ziesunkownemail@clueless.duh>
2014-02-05 10:39:43 -0700

    the ode module now implements the euler function correctly

--- ode.py
+++ ode.py
@@ -1,6 +1,6 @@
 # The ODE (ordinary differential equations) module contains eulers method.
 import numpy as np
 
-def euler(f, x, dt):
-    return f(x) * dt + x
+def euler(f, x, dt, t):
+    return f(x, t) * dt + x
 
